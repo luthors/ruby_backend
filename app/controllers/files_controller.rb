7d59180bb55448f1574aa2324b83c96d72502c90
Luis Toro <luthors@hotmail.com>
2024-04-25 00:34:03 -0500

    endpoints adicionales 25042024-0033

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -44,8 +44,38 @@ class FilesController < ApplicationController
         print("===================JSON=========================== \n")
         puts (response).to_json
         return render :json => response.to_json
-
     end
 
+    def delete_file
+        client = Savon.client(wsdl: "http://172.171.240.20:8000/files/soap/?wsdl")  # Consumir un servicio en formato SOAP
+        token= params[:token]
+        fileId= params[:fileId]
+        response = client.call(:delete_file,message: {
+            "token" => token,
+            "fileId" => fileId
+        })
+        print("===================XML=========================== \n")
+        puts response
+        print("===================JSON=========================== \n")
+        puts (response).to_json
+        return render :json => response.to_json
+    end
+
+    def share_file
+        client = Savon.client(wsdl: "http://172.171.240.20:8000/files/soap/?wsdl")  # Consumir un servicio en formato SOAP
+        token= params[:token]
+        fileId= params[:fileId]
+        user= params[:user]
+        response = client.call(:share_file,message: {
+            "token" => token,
+            "fileId" => fileId,
+            "userShare" => user
+        })
+        print("===================XML=========================== \n")
+        puts response
+        print("===================JSON=========================== \n")
+        puts (response).to_json
+        return render :json => response.to_json
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,22 @@ Rails.application.routes.draw do
   post '/loginsoap', to: 'logins#login_soap'
   post '/register', to: 'logins#register_soap'
 
+  post '/folders/registerfoldersoap', to: 'folders#register_folder_soap'
   get '/folders', to: 'folders#list_folder'
   get '/folders/get_folders_by_parent_id', to: 'folders#get_folders_by_parent_id'
   put '/folders/update_folder_soap', to: 'folders#update_folder_soap'
   put '/folders/share_folder_soap', to: 'folders#share_folder_soap'
   delete '/folders/delete_folder_soap', to: 'folders#delete_folder_soap'
 
-  post '/createfile' => 'files#process_file'
-  put '/updatefile' => 'files#update_file'
-
+  post '/files/createfile' => 'files#process_file'
+  put '/files/updatefile' => 'files#update_file'
+  # share_file
+  put '/files/sharefile' => 'files#share_file'
+  # delete_file
+  delete '/files/deletefile' => 'files#delete_file'
+  # download_file
+  get '/files/downloadfile' => 'files#download_file'
+  
 
 
 
